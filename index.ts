c824322bc9e4bbc4e20cc86260fe05e5f4f1e1f3
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-01-21 19:53:45 +1000

    Added addTupleType decorator to index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ export {VortexStatusService} from "./src/vortex/VortexStatusService";
 export {TupleLoader, IFilterUpdateCallable} from "./src/vortex/TupleLoader";
 export {PayloadEndpoint} from "./src/vortex/PayloadEndpoint";
 export {Payload, IPayloadFilt} from "./src/vortex/Payload";
-export {Tuple, TupleChangeI} from "./src/vortex/Tuple";
+export {Tuple, TupleChangeI, addTupleType} from "./src/vortex/Tuple";
 export {ComponentLifecycleEventEmitter} from "./src/vortex/ComponentLifecycleEventEmitter"
 export * from "./src/vortex/PayloadFilterKeys";
 
