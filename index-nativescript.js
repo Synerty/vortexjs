0e150932c7fc084d8a8779b00dc32c0290ba4684
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-12-07 20:20:34 +1000

    Removed setting of NS worker for payload

--- index-nativescript.js
+++ index-nativescript.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Payload_1 = require("./src/vortex/Payload");
 var PayloadDelegateNs_1 = require("./src/vortex/payload/PayloadDelegateNs");
+exports.PayloadDelegateNs = PayloadDelegateNs_1.PayloadDelegateNs;
 var WebSqlNativeScriptAdaptorService_1 = require("./src/websql/WebSqlNativeScriptAdaptorService");
 exports.WebSqlNativeScriptFactoryService = WebSqlNativeScriptAdaptorService_1.WebSqlNativeScriptFactoryService;
 var TupleStorageFactoryServiceNs_1 = require("./src/vortex/storage-factory/TupleStorageFactoryServiceNs");
 exports.TupleStorageFactoryServiceNs = TupleStorageFactoryServiceNs_1.TupleStorageFactoryServiceNs;
-Payload_1.Payload.setWorkerDelegate(new PayloadDelegateNs_1.PayloadDelegateNs());
 //# sourceMappingURL=/Users/jchesney/project/vortexjs/index-nativescript.js.map
--- index-nativescript.ts
+++ index-nativescript.ts
@@ -1,10 +1,7 @@
-import {Payload} from "./src/vortex/Payload";
-import {PayloadDelegateNs} from "./src/vortex/payload/PayloadDelegateNs";
+export {PayloadDelegateNs} from "./src/vortex/payload/PayloadDelegateNs";
 
 export {WebSqlNativeScriptFactoryService} from "./src/websql/WebSqlNativeScriptAdaptorService";
 
 export {
     TupleStorageFactoryServiceNs
 } from "./src/vortex/storage-factory/TupleStorageFactoryServiceNs";
-
-Payload.setWorkerDelegate(new PayloadDelegateNs());
