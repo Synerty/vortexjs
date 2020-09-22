0f8395d6decf75a5e61d2663fdc1a14fafd383a1
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-09-23 09:03:28 +1000

    ORPEEK-121 Remove NativeScript support

--- index-nativescript.ts
+++
@@ -1,9 +0,0 @@
-export {PayloadDelegateNs} from "./src/vortex/payload/PayloadDelegateNs";
-
-export {WebSqlNativeScriptFactoryService} from "./src/websql/WebSqlNativeScriptAdaptorService";
-export {WebSqlNativeScriptThreadedFactoryService} from "./src/websql/WebSqlNativeScriptThreadedAdaptorService";
-
-export {
-    TupleStorageFactoryServiceNs
-} from "./src/vortex/storage-factory/TupleStorageFactoryServiceNs";
-
